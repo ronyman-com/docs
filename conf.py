4e371bb60b7458ab0018eab7c74f7dd8fa3bb603
Rony Man <ronyman.com@outlook.com>
2022-02-28 20:25:57 +1100

    Update conf.py

--- conf.py
+++ conf.py
@@ -58,11 +58,10 @@ html_theme = 'furo'
 html_static_path = ['_static']
 
 html_sidebars = [
-    "sidebar/scroll-start.html",
-    "sidebar/brand.html",
     "sidebar/search.html",
-    "sidebar/navigation.html",
-    "sidebar/ethical-ads.html",
-    "sidebar/scroll-end.html",
+    "sidebar/getstarted.html",
+    "sidebar/installapps.html",
+    "sidebar/amazons3.html",
+    "sidebar/error_solved.html",
 ]
 
